0c3b088ef457f4159dfb285d75a3580839592fc0
David Dos Reis Inacio <davidinacio97@hotmail.ch>
2024-05-29 14:32:56 +0000

    save

--- labs/05-native-images-graalvm/native-image-gradle-demo/src/main/java/com/thomasvitale/demo/Application.java
+++ labs/05-native-images-graalvm/native-image-gradle-demo/src/main/java/com/thomasvitale/demo/Application.java
@@ -1,8 +1,12 @@
 package com.thomasvitale.demo;
 
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.RouterFunctions;
 import org.springframework.web.servlet.function.ServerResponse;
@@ -14,11 +18,19 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
+
 	@Bean
 	RouterFunction<ServerResponse> routerFunction() {
 		return RouterFunctions.route()
 			.GET("/", request -> ServerResponse.ok().body("Welcome to your development environment!"))
+			.GET("/books", request -> ServerResponse.ok().body(List.of(
+				new Book("The Hobbit"),
+				new Book("The Lord of the Rings")
+			)))
 			.build();
 	}
 
+
+	record Book(String title) {
+	}
 }
